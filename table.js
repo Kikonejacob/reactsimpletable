ec25c79468152e943d9968a4910486bab9c3b06a
Kiswendsida Kikone <kikonejacob@yahoo.fr>
2015-09-05 18:07:06 -0500

    Update table.js

--- table.js
+++ table.js
@@ -1,1 +1,57 @@
 import React from 'react';
+
+var
+  components=[];
+  
+  
+class TableManager{
+  
+  radio={this.props.radio} columns={this.props.columns} 
+  
+  registerComponent(type,name,component)
+  {
+    
+    
+  }
+  
+  getComponent(type,name)
+  {
+    
+    
+  }
+  
+  
+}
+
+
+var table=react.createClass({
+  getInitialState(){
+   
+  
+    
+    
+  }
+  
+  getHeader(){
+    
+      
+      TableHeader=tablemgr.getcomponent('column',this.props.headerComponent)
+    
+    return()
+  }
+  
+  
+  
+  
+  render(){
+    
+  
+    
+    return(
+      
+      <TableHeader  radio={this.props.radio} columns={this.props.columns}  />
+      <TableBody radio={this.props.radio} columns={this.props.columns} />
+      <TableFoot radio={this.props.radio} columns={this.props.columns}  />
+    
+     )
+})
